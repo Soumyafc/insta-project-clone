f2506bb7b0f482aee06532eb090f1cbc62d59ff6
Soumya Ranjan Parida <soumyafc812@gmail.com>
2023-12-03 20:07:48 +0530

    package.json changed due to deployment errors

--- frontend/src/awsbackendkey.js
+++ frontend/src/awsbackendkey.js
@@ -1,3 +1,3 @@
 module.exports = {
-  localhosturl: "http://ec2-13-232-43-38.ap-south-1.compute.amazonaws.com:5000"
+  localhosturl: "http://ec2-13-232-43-38.ap-south-1.compute.amazonaws.com:3888"
 };
